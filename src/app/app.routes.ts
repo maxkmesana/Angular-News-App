24646c19fe248ccba4f4406330e918944ee11e7e
Max <max.kmesana@gmail.com>
2024-10-26 23:58:19 -0300

    Finished basic visual article component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ArticleCardComponent } from './components/article-card/article-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"dev-article-card", component: ArticleCardComponent
+    }
+];

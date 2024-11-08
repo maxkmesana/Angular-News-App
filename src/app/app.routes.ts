7777ccedf702942bfeed533b921ebe0557dd5d33
tinaurquiza <urquiza.val@gmail.com>
2024-11-08 09:55:13 -0300

    article view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,23 @@ import { ArticleCardComponent } from './components/article-card/article-card.com
 import { ArticleListPageComponent } from './pages/article-list-page/article-list-page.component';
 import { MyNewsComponent } from './components/my-news/my-news.component';
 import { MyNewsPageComponent } from './pages/my-news-page/my-news-page.component';
-
+import { ArticlePageComponent } from './pages/article-page/article-page.component';
 
 export const routes: Routes = [
-    {
-        path:"dev-article-card", component: ArticleCardComponent
-    },
-    {
-        path:"dev-article-list-page", component: ArticleListPageComponent,
-    },
-    {
-        path:"my-news", component: MyNewsPageComponent,
-    }
+  {
+    path: 'dev-article-card',
+    component: ArticleCardComponent,
+  },
+  {
+    path: 'dev-article-list-page',
+    component: ArticleListPageComponent,
+  },
+  {
+    path: 'my-news',
+    component: MyNewsPageComponent,
+  },
+  {
+    path: 'articles/:id',
+    component: ArticlePageComponent,
+  },
 ];
---
+++ src/app/components/article-view/article-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './article-view.component.html',
+  styleUrl: './article-view.component.css'
+})
+export class ArticleViewComponent {
+
+}

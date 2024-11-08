476ea92a76ac64806f2a385c730a9f22ad0278ef
tinaurquiza <urquiza.val@gmail.com>
2024-11-08 09:58:04 -0300

    article view created

--- src/app/components/article-view/article-view.component.ts
+++ src/app/components/article-view/article-view.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../shared/nav/nav.component';
 
 @Component({
   selector: 'app-article-view',
   standalone: true,
-  imports: [],
+  imports: [NavComponent, ArticleViewComponent],
   templateUrl: './article-view.component.html',
-  styleUrl: './article-view.component.css'
+  styleUrl: './article-view.component.css',
 })
-export class ArticleViewComponent {
-
-}
+export class ArticleViewComponent {}

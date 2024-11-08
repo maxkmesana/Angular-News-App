148557ee6a1100a41a9b5d0cb59e61ceafa9b892
tinaurquiza <urquiza.val@gmail.com>
2024-11-08 10:22:20 -0300

    article view

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../shared/nav/nav.component';
+import { ArticleViewComponent } from '../../components/article-view/article-view.component';
 
 @Component({
   selector: 'app-article-page',
   standalone: true,
-  imports: [],
+  imports: [NavComponent, ArticleViewComponent],
   templateUrl: './article-page.component.html',
-  styleUrl: './article-page.component.css'
+  styleUrl: './article-page.component.css',
 })
-export class ArticlePageComponent {
-
-}
+export class ArticlePageComponent {}

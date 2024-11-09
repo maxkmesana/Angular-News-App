11aaa82feb20768ff3b4c165a4b105ef5a4051a6
tinaurquiza <urquiza.val@gmail.com>
2024-11-09 18:46:00 -0300

    cursor pointer add on favorite card

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../shared/nav/nav.component';
 import { ArticleViewComponent } from '../../components/article-view/article-view.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-article-page',
   standalone: true,
-  imports: [NavComponent, ArticleViewComponent],
+  imports: [NavComponent, ArticleViewComponent, FooterComponent],
   templateUrl: './article-page.component.html',
   styleUrl: './article-page.component.css',
 })
--- src/app/pages/my-news-page/my-news-page.component.ts
+++ src/app/pages/my-news-page/my-news-page.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../shared/nav/nav.component';
 import { MyNewsComponent } from '../../components/my-news/my-news.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-my-news-page',
   standalone: true,
-  imports: [NavComponent, MyNewsComponent],
+  imports: [NavComponent, MyNewsComponent, FooterComponent],
   templateUrl: './my-news-page.component.html',
-  styleUrl: './my-news-page.component.css'
+  styleUrl: './my-news-page.component.css',
 })
-export class MyNewsPageComponent {
-
-}
+export class MyNewsPageComponent {}

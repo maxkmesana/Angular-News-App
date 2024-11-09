9f143a550b7ea85d738a876816b63bd6082a1020
tinaurquiza <urquiza.val@gmail.com>
2024-11-09 18:18:46 -0300

    burguer-menu

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -6,8 +6,41 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   standalone: true,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
 })
 export class NavComponent {
+  isMenuOpen = false;
 
+  // Función para alternar el menú
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    // Prevenir el scroll cuando el menú está abierto
+    if (this.isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }
+
+  // Función para cerrar el menú
+  closeMenu(): void {
+    this.isMenuOpen = false;
+    document.body.style.overflow = 'auto';
+  }
+
+  // Cerrar el menú cuando la ventana se redimensiona
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if (window.innerWidth > 768 && this.isMenuOpen) {
+      this.closeMenu();
+    }
+  }
+
+  // Cerrar el menú cuando se presiona la tecla Escape
+  @HostListener('document:keydown.escape', ['$event'])
+  onKeydownHandler() {
+    if (this.isMenuOpen) {
+      this.closeMenu();
+    }
+  }
 }
